a3181111f2e517aa8ffdc74f8969f72e31de3a45
unknown <you@example.com>
2024-10-07 22:15:34 -0300

    feat: Add default category seeder on application startup

---
+++ src/main/java/com/qima/product_management_system/config/DatabaseSeeder.java
@@ -0,0 +1,25 @@
+package com.qima.product_management_system.config;
+
+import com.qima.product_management_system.model.Category;
+import com.qima.product_management_system.repository.CategoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseSeeder {
+
+    @Bean
+    CommandLineRunner initDatabase(CategoryRepository categoryRepository) {
+        return args -> {
+            if (categoryRepository.count() == 0) { // Verifica se o banco está vazio
+                categoryRepository.save(new Category(null, "Electronics", null));
+                categoryRepository.save(new Category(null, "Books", null));
+                categoryRepository.save(new Category(null, "Furniture", null));
+                categoryRepository.save(new Category(null, "Clothing", null));
+                categoryRepository.save(new Category(null, "Toys",null));
+                System.out.println("Default categories seeded into the database.");
+            }
+        };
+    }
+}
